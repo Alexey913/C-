96999b05baff8d8c8319009e0378c601d83b0a2c
Alexey Shevtsov <al913@mail.ru>
2022-08-11 09:38:50 +0300

    gitignore

---
+++ Task10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите трехзначное число, и я покажу его вторую цифру");
+int a = Convert.ToInt32 (Console.ReadLine());
+if ((a>99 && a<1000) || (a>-1000 && a<-99))
+{
+    int second = Math.Abs((a-a/100*100-a%10)/10);
+    Console.WriteLine ($"Вторая цифра числа равна {second}");
+}
+else
+{
+    Console.WriteLine ("Число не является трехзначным ");
+}
