bb82587be98b537c2951a6212c254f56f8d718a6
Alexey Shevtsov <al913@mail.ru>
2022-08-11 21:54:08 +0300

    All tasks

---
+++ 22Square/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите число N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (n>0)
+{
+    while (count<n)
+    {
+        Console.Write ($"{count*count}, ");
+        count++;
+    }
+    Console.WriteLine(n*n);
+}
+else if (n<0)
+{
+    while (count>n)
+    {
+        Console.Write ($"{count*count}, ");
+        count= count-1;
+    }
+    Console.WriteLine(n*n);
+
+}
+else
+{
+    Console.WriteLine (0);
+}
