e1da6b56a0d3f59d11bd29e366cc9fcdfefe2d56
Alexey Shevtsov <al913@mail.ru>
2022-08-12 19:53:06 +0300

    Task10 Finn second digit 3-discharge number

--- Task10/Program.cs
+++ Task10/Program.cs
@@ -1,8 +1,9 @@
 ﻿Console.WriteLine("Введите трехзначное число, и я покажу его вторую цифру");
 int a = Convert.ToInt32 (Console.ReadLine());
-if ((a>99 && a<1000) || (a>-1000 && a<-99))
+a = Math.Abs (a);
+if (a>99 && a<1000)
 {
-    int second = Math.Abs((a-a/100*100-a%10)/10);
+    int second = (a-a/100*100-a%10)/10;
     Console.WriteLine ($"Вторая цифра числа равна {second}");
 }
 else
