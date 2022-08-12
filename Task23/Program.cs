a67c58c593aedf4753c96e847db2c02218c76012
Alexey Shevtsov <al913@mail.ru>
2022-08-12 21:10:50 +0300

    replaced

--- Task23/Program.cs
+++ Task23/Program.cs
@@ -1,11 +1,31 @@
-﻿Console.WriteLine("Введите координату x первой точки:");
-double xa = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату y первой точки:");
-double ya = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату x второй точки:");
-double xb = Convert.ToDouble(Console.ReadLine());
-Console.WriteLine("Введите координату y второй точки:");
-double yb = Convert.ToDouble(Console.ReadLine());
-double dist = 0;
-dist = Math.Sqrt (Math.Pow(xa-xb,2)+Math.Pow(ya-yb,2));
-Console.WriteLine ($"Расстояние между точками равно {dist}");
+﻿Console.WriteLine("Введите число N, и я выведу таблицу кубов от 1 до N");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if (n>0)
+{
+    while (count<n)  // использую строгое неравенство, чтобы осуществить вывод через запятую, как в примере к задаче
+                     // и при этом избавиться от запятой в конце ряда
+                     // можно заменить строки 10, 11, 12 на Console.WriteLine ($"{Math.Pow(count,3)}, "),
+                     //поставив в условии нестрогое неравенство
+    {
+        Console.Write ($"{Math.Pow(count,3)}, ");
+        count++;
+    }
+    Console.WriteLine(Math.Pow(count,3));
+}
+else if (n<0)
+{
+    while (count>n)  // то же, что в строке 6, для возможности использования отрицательных чисел
+                     // при этом выводится тот же ряд, что и для положительных,
+                     // только внчале добавляем 1 и 0, чтобы соблюсти условие задачи (от 1 до N)
+    {
+        Console.Write ($"{Math.Pow(count,3)}, ");
+        count = count-1;
+    }
+    Console.WriteLine(Math.Pow(count,3));
+
+}
+else
+{
+    Console.WriteLine (0);
+}
