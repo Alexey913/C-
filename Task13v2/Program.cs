6aa91abd833f5528a7a53ebf2cacf264784c6a53
Alexey Shevtsov <al913@mail.ru>
2022-08-12 20:05:35 +0300

    Find 3 digit of number - into string

---
+++ Task13v2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine ("Введите любое число, и я покажу его третью цифру:");
+int a = Convert.ToInt32 (Console.ReadLine());
+a = Math.Abs (a);
+string b = Convert.ToString(a);
+
+if ((a<100))
+{
+    Console.WriteLine ("В числе нет третьей цифры!");
+}
+
+else 
+{
+int third = int.Parse (b[2].ToString());
+Console.WriteLine($"Третья цифра равна {third}");
+}
