24620482a71bf46857833eddade3d8a460ac469f
Alexey Shevtsov <al913@mail.ru>
2022-08-14 21:45:02 +0300

    Replace symbols

---
+++ Example003/Program.cs
@@ -0,0 +1,37 @@
+﻿//=====Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить чёрточками,
+// маленькие буквы “к” заменить большими “К”,
+// а большие “С” маленькими “с”.
+// Ясна ли задача?
+
+string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
++ "ежели бы вас послали вместо нашего милого Винценгероде, "
++ "вы бы взяли приступом согласие прусского короля. "
++ "Вы так красноречивы. Вы дадите мне чаю?";
+
+// string s = “qwerty”
+//             012345
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+    int length = text.Length;
+    for (int i = 0; i < length; i++)
+    {
+        if (text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace (text, ' ', '_');
+Console.WriteLine(text);
+Console.WriteLine();
+Console.WriteLine(newText);
+
+string newText2 = Replace (newText, 'к', 'К');
+Console.WriteLine();
+Console.WriteLine(newText2);
+
+string newText3 = Replace (newText2, 'В', 'в');
+Console.WriteLine();
+Console.WriteLine(newText3);
