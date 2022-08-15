ce3dbb1ecb436e646f322c12f081152b04df4469
Alexey Shevtsov <al913@mail.ru>
2022-08-15 22:57:56 +0300

    Example002 How much digits in number

---
+++ Example002/Program.cs
@@ -0,0 +1,32 @@
+﻿//Сколько цифр в числе?
+
+
+//int metod (int n)
+//{
+//    string num = Convert.ToString(n);
+//    int length = num.Length;
+//    return length;
+//}
+
+//Console.WriteLine ("Введите число:");
+//int number = Convert.ToInt32(Console.ReadLine());
+//int abs = Math.Abs (number);
+//int length = metod (abs);
+//Console.WriteLine (length);
+
+
+
+
+Console.WriteLine ("Введите число:");
+int number = Convert.ToInt32(Console.ReadLine());
+int abs = Math.Abs (number);
+int count = 0;
+while (abs>0)
+{
+    abs = abs/10;
+    count = count+1;
+}
+
+Console.WriteLine (count);
+
+
