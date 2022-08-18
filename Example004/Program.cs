5dd2ff40fd2f6e0d27c55da978145e05057818ae
Alexey Shevtsov <al913@mail.ru>
2022-08-18 22:41:56 +0300

    Example004 Array Multiplicate 1 and last

---
+++ Example004/Program.cs
@@ -0,0 +1,36 @@
+﻿Console.Write ("Задайте размерность массива:");
+int discharge = Convert.ToInt32 (Console.ReadLine());
+int [] array = new int [discharge];
+int N = 0;
+Random rand = new Random();
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=rand.Next(-9,10);
+}
+Console.WriteLine ('['+string.Join(", ", array)+']');
+if (discharge%2 == 0)
+{
+    N=0;
+}
+else
+{
+    N=1;
+}
+int [] result = new int[array.Length/2+N];
+if (array.Length%2==0)
+   {
+        for (int i=0; i<array.Length/2; i++)
+        {
+            result[i]=array[i]*array[array.Length-1-i];
+        }
+    }
+else
+    {
+        for (int i=0; i<array.Length/2; i++)
+        {
+            result[i]=array[i]*array[array.Length-1-i];
+        
+            result[array.Length/2]=array[array.Length/2];
+        }
+    }
+Console.WriteLine ('['+string.Join(", ", result)+']');
