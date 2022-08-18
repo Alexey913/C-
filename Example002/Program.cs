b3e1c860c9156f5cb66efb6de68931bf13faf353
Alexey Shevtsov <al913@mail.ru>
2022-08-18 22:45:32 +0300

    Change + on - and - on + in array

---
+++ Example002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write ("Задайте размерность массива:");
+int discharge = Convert.ToInt32 (Console.ReadLine());
+int [] array = new int [discharge];
+Random rand = new Random();
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=rand.Next(-1000,1000);
+}
+Console.WriteLine ('['+string.Join(", ", array)+']');
+for (int i=0; i<array.Length; i++)
+{
+    array [i]=-array[i];
+}
+Console.WriteLine ('['+string.Join(", ", array)+']');
