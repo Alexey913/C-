5106f36b6f37962c845c66efc2219cd5b8d5ec35
Alexey Shevtsov <al913@mail.ru>
2022-08-18 22:46:22 +0300

    Sum positive and negaitve elements of array

---
+++ Example001/Program.cs
@@ -0,0 +1,21 @@
+﻿int [] array = new int [12];
+int sumPositive = 0;
+int sumNegative = 0;
+Random rand = new Random();
+for (int i=0; i<array.Length; i++)
+{
+    array[i]=rand.Next(-9,10);
+}
+Console.WriteLine ('['+string.Join(", ", array)+']');
+for (int i=0; i<array.Length; i++)
+{
+    if (array [i]>0)
+    {
+        sumPositive+=array[i];
+    }
+    else
+    {
+        sumNegative+=array[i];
+    }
+}
+Console.WriteLine ($"Сумма положительных элементов равна {sumPositive}, а сумма отрицательных элементов - {sumNegative}");
