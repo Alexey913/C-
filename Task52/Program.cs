3a21b17e8dec589cc8c8dc9a99b059b2e514058c
Alexey Shevtsov <al913@mail.ru>
2022-08-27 11:58:07 +0300

    Task52 Arithmetic mean of array columns

---
+++ Task52/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+
+Console.WriteLine("Введите количество строк и столбцов массива, и я заполню его элементами, равными сумме своих индексов");
+Console.WriteLine("Сколько будет строк?");
+int m = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine("Сколько будет столбцов?");
+int n = Convert.ToInt32 (Console.ReadLine());
+Console.WriteLine();
+int [,] array = new int [m,n];
+double [] srednee = new double [n];
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array [i,j] = new Random().Next (0, 11);
+        Console.Write(array[i,j]+" ");
+        srednee[j] = srednee[j] + array [i,j];
+    }
+    Console.WriteLine();
+}
+
+for (int j = 0; j < n; j++)
+{
+    srednee [j] = Math.Round(srednee [j]/m,1);
+}
+Console.WriteLine();
+Console.WriteLine("Средние арифметические каждого столбца равны:");
+Console.WriteLine('['+String.Join(", ", srednee)+']');
