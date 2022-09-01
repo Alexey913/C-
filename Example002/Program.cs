b2f26e92ef571fa87d57b151e103999c783b37a5
Alexey Shevtsov <al913@mail.ru>
2022-09-01 21:19:15 +0300

    Example002 Numbers from M to N with RECURSION

---
+++ Example002/Program.cs
@@ -0,0 +1,46 @@
+﻿void PrintNumbers (int M, int N)
+{
+    
+    // int min = M;
+    // int max = N;
+    // if (M > N)
+    // {
+    //     min = N;
+    //     max = M;
+    // }
+    // if (min == max-1)
+    // {
+    //     return;
+    // }
+    // else 
+    // {
+    //     PrintNumbers(min, max-1);
+    //     Console.Write ($"{max} ");
+    // }
+    
+    if (N == M-1)
+    {
+        return;
+    }
+    else 
+    {
+    PrintNumbers(M, N-1);
+    Console.Write ($"{N} ");
+    }
+}
+
+Console.WriteLine ("Введите числа M и N, и я выведу все натуральные числа от M до N");
+Console.Write ("Число M равно: ");
+int M = Convert.ToInt32 (Console.ReadLine());
+
+Console.Write ("Число N равно: ");
+int N = Convert.ToInt32 (Console.ReadLine());
+
+if (M < N)
+{
+    PrintNumbers(M, N);
+}
+else
+{
+    PrintNumbers(N, M);
+}
