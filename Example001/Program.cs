4bcb4ec2bb76e71e55a091895e533045cc450f0b
Alexey Shevtsov <al913@mail.ru>
2022-09-01 20:28:15 +0300

    Numbers from 1 to N with RECURSION

---
+++ Example001/Program.cs
@@ -0,0 +1,16 @@
+﻿void PrintNumbers (int N)
+{
+    if (N==0)
+    {
+        return;
+    }
+    else 
+    {
+    PrintNumbers(N-1);
+    Console.Write ($"{N} ");
+    }
+}
+Console.WriteLine ("Введите число N, и я выведу все натуральные числа от 1 до N");
+int N = Convert.ToInt32 (Console.ReadLine());
+
+PrintNumbers(N);
